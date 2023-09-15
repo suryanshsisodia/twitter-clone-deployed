8a0a77e791604a23ce383634a2a772c9d8a0bcf8
Suryansh <suryansh20614@iiitkalyani.ac.in>
2023-09-15 21:40:24 +0530

    commit for deployment

--- backend/index.js
+++ backend/index.js
@@ -50,6 +50,24 @@ app.use("/auth", authRoutes); //to Login
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+//----------------Deployment--------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV==='production') {
+  app.use(express.static(path.join(__dirname1, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
+//----------------------------------------
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
